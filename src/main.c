8f5751598a3c66e3d9c5bfb5e8ce2fc142d225ff
DuckDood <b.c.youtub@gmail.com>
2025-06-19 15:05:32 +1200

    Useless commit message

--- src/main.c
+++ src/main.c
@@ -6,6 +6,7 @@
 #include <menu.h>
 #include <dirent.h>
 #include <pthread.h>
+#include <math.h>
 #include <unistd.h>
 
 #define SIZE_ARRAY(a) (sizeof(a)/sizeof(a[0]))
@@ -15,12 +16,20 @@ void sigHandler(int s) {
 	quick_exit(0);
 }
 
+Mix_Music* music;
+bool willloop = false;
+
+void loop() {
+	if(willloop) 
+		Mix_PlayMusic(music, 0);
+}
+
 char **choices = {
 };
 
 
 int main(int argc, char** argv) {
-	Mix_Music* music;
+	//printf("");
 	SDL_Init(SDL_INIT_AUDIO);
 	Mix_Init(MIX_INIT_MP3);
 	DIR* dir;
@@ -40,7 +49,6 @@ int main(int argc, char** argv) {
 	int c = 12;
 	MENU* menu;
 	ITEM * cur_it;
-	pthread_t thread;
 	signal(SIGQUIT, sigHandler);
 	initscr();
 	cbreak();
@@ -50,11 +58,13 @@ int main(int argc, char** argv) {
 	WINDOW *main;
 	WINDOW *stat;
 	nodelay(stdscr, true);
-	char*playingSong = calloc(1, 1);
+	int half = COLS/3-2;
+	char*playingSong = "(None)";
 	refresh();
 	int width = COLS/3, height = LINES/1.3, x = (COLS-width)/2, y = (LINES-height)/2;
 	main = newwin(height, width, y, x);
-	stat = newwin(6, 50, LINES-6, 0);
+	//stat = newwin(6, 50, LINES-6, 0);
+	stat = newwin(4, half+2, LINES-4, 0);
 	box(main, 0, 0);
 
 	wrefresh(main);
@@ -72,7 +82,7 @@ int main(int argc, char** argv) {
 	set_menu_sub(menu, derwin(main, 0, 0, 1, 0));
 	set_menu_pad(menu, 1);
 
-	char* mspath;
+	char* mspath = 0;
 	
 	post_menu(menu);
 	wrefresh(main);
@@ -96,14 +106,46 @@ int main(int argc, char** argv) {
 	int framecount = 0;
 	bool ends = false;
 	int count = 0;
-	
+
+	WINDOW* textbox;
+
+	float msicdur;
+	float pos;
+	float absPos = -1;
+	char name[50];
+	char *shellcmd;
+	Mix_HookMusicFinished(loop);
+	start_color();
+	use_default_colors();
+	init_pair(1, COLOR_WHITE, COLOR_WHITE);
+	init_pair(2, COLOR_GREEN, -1);
 	while(true) {
+		if(willloop) {
+			mvprintw(1, 0, "Looping    ");
+		} else {
+			mvprintw(1, 0, "Not Looping");
+		}
+		if(Mix_PlayingMusic()) {
+		absPos = Mix_GetMusicPosition(music);
+		}
+		if(absPos != -1) {
+		pos = absPos/msicdur;
+		}
+				
 		framecount++;
 		werase(stat);
 		wborder(stat, 0,0,0,0, 0,0,0,0);
-		if(strlen(playingSong) < 48) {
+	//	mvwprintw(stat, 2, 1, "________________________________________________");
+		for(int j = 0; j<half; j++) {
+			mvwprintw(stat, 2, 1+j, "_");
+		}
+		//mvwprintw(stat, 2, 1+pos*48, "#");
+		wattron(stat, COLOR_PAIR(1));
+		mvwprintw(stat, 2, 1+pos*half, "#");
+		wattroff(stat, COLOR_PAIR(1));
+		if(strlen(playingSong) < half) {
 			ends = false;
-		mvwprintw(stat, 1, 1, "%s", playingSong);
+				mvwprintw(stat, 1, 1, "%s", playingSong);
 		} else {
 			/*
 			if(!ends) {
@@ -118,7 +160,7 @@ int main(int argc, char** argv) {
 			}*/
 			//usleep(100000);
 			
-			if(scrollamnt > strlen(playingSong)-47){
+			if(scrollamnt > strlen(playingSong)-(half-4)){
 			if(!ends) {
 				ends = true;
 				count = 0;
@@ -149,7 +191,7 @@ int main(int argc, char** argv) {
 				}
 				framecount = 0;
 			}
-			strncpy(scrollcname, playingSong+scrollamnt, 47);
+			strncpy(scrollcname, playingSong+scrollamnt, half-5);
 			mvwprintw(stat, 1, 1, "%s", scrollcname);
 
 		}
@@ -157,7 +199,9 @@ int main(int argc, char** argv) {
 		wrefresh(stat);
 		//if(c!=ERR) {
 		attron(A_ITALIC);
+		attron(COLOR_PAIR(2));
 		mvprintw(0, 0, "Stopify");
+		attroff(COLOR_PAIR(2));
 		attroff(A_ITALIC);
 		
 
@@ -180,9 +224,23 @@ int main(int argc, char** argv) {
 				menu_driver(menu, REQ_UP_ITEM);
 				break;
 
+			case KEY_LEFT:
 			case 'a': 
+				if(Mix_PlayingMusic()) {
+					Mix_SetMusicPosition(fmin(fmax(absPos-5, 0), msicdur));
+				} else {
+				if(mspath == 0) break;
+
+				music = Mix_LoadMUS(mspath);
+				Mix_PlayMusic(music, 0);
+				Mix_SetMusicPosition(fmax(msicdur-5, 0));
+
+
+				}
 				break;
+			case KEY_RIGHT:
 			case 'd': 
+				Mix_SetMusicPosition(fmin(fmax(absPos+5, 0), msicdur));
 				break;
 			case '\n':
 				playingSong=malloc(strlen(item_name(current_item(menu)))+1);
@@ -194,6 +252,8 @@ int main(int argc, char** argv) {
 				//printw("%s", mspath);
 				music = Mix_LoadMUS(mspath);
 				Mix_PlayMusic(music, 0);
+				msicdur = Mix_MusicDuration(music);
+				pos = Mix_GetMusicPosition(music)/msicdur;
 				break;
 			case ' ': 
 				if(Mix_PausedMusic()) {
@@ -201,6 +261,38 @@ int main(int argc, char** argv) {
 				} else {
 					Mix_PauseMusic();
 				}
+				break;
+			case 'c':
+			/*	textbox = newwin(3, 40, LINES/2-2, COLS/2-20);
+				box(textbox, 0, 0);
+				wrefresh(textbox);
+				//nodelay();
+				echo();
+				//mvwscanw(textbox,1,1, "");
+				mvwgetnstr(textbox,1,1,name,39);
+				noecho();
+				shellcmd = malloc(39+6+1+2+1+strlen(item_name(current_item(menu)))+1+strlen(argv[1])*2+1+1+1+12);
+				strcpy(shellcmd, "ffmpeg -i ");
+				strcat(shellcmd, argv[1]);
+				strcat(shellcmd, "/");
+				strcat(shellcmd, item_name(current_item(menu)));
+				strcat(shellcmd, " ");
+				strcat(shellcmd, argv[1]);
+				strcat(shellcmd, "/");
+				strcat(shellcmd, name);
+				strcat(shellcmd, " &> /dev/null");
+				system(shellcmd);
+				delwin(textbox);
+
+
+				goto start;
+
+*/
+				// converts but doesnt fit the program
+				break;
+
+			case 'l':
+				willloop = !willloop;
 				break;
 			default:
 			break;
